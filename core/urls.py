e93a1e976524a40e676536469ea3676a7ee3cee8
Flashdaniel <49584202+Flashdaniel@users.noreply.github.com>
2021-08-04 21:57:01 +0100

    Added schema and coreapi to project

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,4 +15,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include('blog.urls',  namespace='blog')),
+    path('doc/', include_docs_urls(title='BlogAPI')),
+    path('schema', get_schema_view(
+        title="BlogApi",
+        description="API on the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
